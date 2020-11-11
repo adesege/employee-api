85d748df05983042110c22ef55158fe07614642e
Fadojutimi Temitayo Olusegun <Fadojutimitemitayo@gmail.com>
2020-11-11 23:54:16 +0100

    ch: cleanup unused modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ServicesModule } from './services/services.module';
 
 @Module({
   imports: [ServicesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
