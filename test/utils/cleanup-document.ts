dc4d4f938bfea576e9a4480a6dd68dd92d7bfda9
Fadojutimi Temitayo Olusegun <Fadojutimitemitayo@gmail.com>
2020-11-13 00:33:17 +0100

    ch: close database connection

--- test/utils/cleanup-document.ts
+++ test/utils/cleanup-document.ts
@@ -4,5 +4,6 @@ import { Connection } from "mongoose";
 
 export const cleanupDocuments = async (app: INestApplication): Promise<any> => {
   const connection = app.get<Connection>(getConnectionToken());
-  return connection.dropDatabase();
+  await connection.dropDatabase();
+  return connection.close()
 }
