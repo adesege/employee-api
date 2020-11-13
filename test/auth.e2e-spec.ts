32299e3b95fc5ec74c0afa56366ea9e2d5807e9a
Fadojutimi Temitayo Olusegun <Fadojutimitemitayo@gmail.com>
2020-11-13 05:20:35 +0100

    ch: add e2e tests for deactivated employee

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,15 +1,20 @@
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { Test, TestingModule } from '@nestjs/testing';
 import { configureApp } from 'configure-app';
+import { RolesEnum } from 'enums/roles.enum';
+import { StatusEnum } from 'enums/status.enum';
+import { UserDocument } from 'schemas/user.schema';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { userMock } from './mocks/user.mock';
 import { cleanupDocuments } from './utils/cleanup-document';
-import { createUser } from './utils/user';
+import { createUser, signToken } from './utils/user';
 
 describe('AuthController (e2e)', () => {
   let app: NestExpressApplication;
+  let deactivatedUserToken: string;
   const user = userMock();
+  const deactivatedUser = userMock();
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -21,6 +26,12 @@ describe('AuthController (e2e)', () => {
     await app.init();
 
     await createUser(app, user);
+    const deactivatedUserDocument = await createUser(app, {
+      ...deactivatedUser,
+      status: StatusEnum.DEACTIVATED
+    }) as UserDocument;
+    deactivatedUserToken = await signToken(app, deactivatedUserDocument);
+
   });
 
   afterAll(async () => {
@@ -36,6 +47,8 @@ describe('AuthController (e2e)', () => {
       .expect(200)
       .expect(response => {
         expect(response.body).toHaveProperty('token')
+        expect(response.body.user.roles).toHaveLength(1);
+        expect(response.body.user.roles).toContain(RolesEnum.EMPLOYEE);
         expect(user).toEqual(expect.objectContaining(response.body.user));
       });
   });
@@ -60,5 +73,21 @@ describe('AuthController (e2e)', () => {
       .expect(response => {
         expect(response.body.message).toContain("Not a valid email address")
       });
+  });
+
+  it('should not log a deactivated employee in', () => {
+    return request(app.getHttpServer())
+      .post('/api/auth/signin')
+      .send({ email: deactivatedUser.email, password: deactivatedUser.password })
+      .accept('application/json')
+      .expect(401)
+  });
+
+  it('should not allow a deactivated employee to access protected route', () => {
+    return request(app.getHttpServer())
+      .get('/api/auth/me')
+      .set({ Authorization: `Bearer ${deactivatedUserToken}` })
+      .accept('application/json')
+      .expect(401)
   });
 });
